ee6550bf365e21da04cf216a5f763fa55121a74b
Lutz Fischer <lutz.fischer@tu-berlin.de>
2025-07-24 18:03:56 +0200

    add some cleanup code

--- dirlock/__init__.py
+++ dirlock/__init__.py
@@ -1,6 +1,42 @@
 import os
 import time
+import atexit
 from datetime import datetime
+
+
+# keep a list of currently acquired looks
+# so these can be cleaned up on exit
+_allActiveLocks = set()
+
+# function to clean up all active locks
+def _clean_locks():
+    global _allActiveLock
+    for dl in _allActiveLock:
+        dl.release()
+
+# we don't want to call the original handler
+original_sigint_handler = signal.getsignal(signal.SIGINT)
+original_sigterm_handler = signal.getsignal(signal.SIGTERM)
+
+def handle_sigint_cleanup(signum, frame):
+    global original_sigint_handler
+    _clean_locks()
+    if original_sigint_handler is not None:
+        original_sigint_handler()
+
+def handle_sigterm_cleanup(signum, frame):
+    global original_sigterm_handler
+    _clean_locks()
+    if original_sigterm_handler is not None:
+        original_sigterm_handler()
+
+# normal exit clean up
+atexit.register(cleanlocks)
+
+# ctrl+c cleanup
+signal.signal(signal.SIGINT, handle_sigint_cleanup)
+# sigterm cleanup
+signal.signal(signal.SIGTERM, handle_sigterm_cleanup)
 
 
 class DirLock:
@@ -30,12 +66,14 @@ class DirLock:
         """
         Acquire the lock by following the directory-based lock mechanism.
         """
-        start_time = datetime.now()
+        global _allActiveLock
+        start_time = datetime.now()    
 
         while True:
             try:
                 os.mkdir(self.lock_dir)
                 self.acquired = True
+                _allActiveLocks.add(self)
                 break
             except FileExistsError:
                 pass
@@ -51,8 +89,10 @@ class DirLock:
         """
         Release the lock if it is held by this instance.
         """
+        global _allActiveLock
         try:
             os.rmdir(self.lock_dir)
+            _allActiveLocks.remove(self)
         except FileNotFoundError:
             pass
         self.acquired = False
